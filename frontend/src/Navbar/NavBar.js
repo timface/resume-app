c82892ed89c15ee8fd6b3c3ae7e577c55591e123
Tim <timhart404@gmail.com>
2020-05-26 12:04:48 +1000

    -- unused vars from WorkHistory HistoryCard,+ Nav links

--- frontend/src/Navbar/NavBar.js
+++ frontend/src/Navbar/NavBar.js
@@ -3,10 +3,13 @@ import {Link} from 'react-router-dom';
 
 function NavBar() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
+        <nav className="navbar navbar-expand navbar-left navbar-dark bg-primary fixed-top">
             <Link className="navbar-brand" to="/">
                 Timothy Hart
             </Link>
+            <Link className="nav-item text-white mr-2" to="/workhistory">Employment History</Link>
+            <Link className="nav-item text-white mr-2" to="/eduhistory">Education History</Link>
+            <Link className="nav-item text-white mr-2" to="/skills">Skills</Link>
         </nav>
     );
 }
