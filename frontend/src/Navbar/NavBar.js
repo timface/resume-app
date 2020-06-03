4ee8e671dc009f142a3a9659b29f9f4d62d7b9a2
Tim <timhart404@gmail.com>
2020-06-04 09:55:17 +1000

    Added collapsing navbar

--- frontend/src/Navbar/NavBar.js
+++ frontend/src/Navbar/NavBar.js
@@ -4,15 +4,20 @@ import { Link } from 'react-router-dom';
 function NavBar() {
     return (
         <div className="container fixed-top">
-            <nav className="navbar navbar-expand navbar-left bg-info">
-                <Link className="navbar-brand text-white" to="/">
-                    Timothy Hart
-            </Link>
-                <Link className="nav nav-item nav-link text-white" to="/workhistory">Employment History</Link>
-                <Link className="nav nav-item nav-link text-white" to="/eduhistory">Education History</Link>
-                <Link className="nav nav-item nav-link text-white" to="/skills">Skills</Link>
-                <Link className="nav nav-item nav-link text-white" to="/testimonials">Testimonials</Link>
-                <Link className="nav nav-item nav-link text-white" to='/contact'>Contact Me</Link>
+            <nav className="navbar navbar-expand-lg navbar-left bg-info">
+                <Link className="navbar-brand text-white" to="/">Timothy Hart</Link>
+                <button class="navbar-toggler bg-danger" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse navbar-dark" id="navbarSupportedContent">
+                    <div className="nav navbar-nav">
+                        <Link className="nav nav-item nav-link text-white" to="/workhistory">Employment History</Link>
+                        <Link className="nav nav-item nav-link text-white" to="/eduhistory">Education History</Link>
+                        <Link className="nav nav-item nav-link text-white" to="/skills">Skills</Link>
+                        <Link className="nav nav-item nav-link text-white" to="/testimonials">Testimonials</Link>
+                        <Link className="nav nav-item nav-link text-white" to='/contact'>Contact Me</Link>
+                    </div>
+                </div>
             </nav>
         </div>
     );
